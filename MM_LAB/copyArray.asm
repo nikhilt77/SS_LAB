6d1c4ba56ec192d7e692bac0f8e80fdeb4ae49b3
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-11-12 19:26:20 +0530

    Update copyArray.asm

--- MM_LAB/copyArray.asm
+++ MM_LAB/copyArray.asm
@@ -1,7 +1,8 @@
 data segment 
-    arr1 db 1,2,3,4,5
-    length equ 5  
-    msg db 10,13,'Copied$'
+    msg1 db 10,13,'Enter 5 numbers:$' 
+    msg2 db 10,13,'Copied Array:$'
+    arr1 db 20 dup(?)
+    length equ 5
     arr2 db 20 dup(?)
 data ends
 code segment
@@ -9,10 +10,27 @@ code segment
 start:
     mov ax,data
     mov ds,ax
+    lea dx,msg1
+    mov ah,09h
+    int 21h
     mov cx,length
     lea si,arr1
     lea di,arr2
-copy:         
+input:
+    mov ah,01h
+    int 21h
+    sub al,'0'
+    mov [si],al 
+    inc si
+    dec cx
+    jnz input
+    mov cx,length
+    lea si,arr1
+    lea di,arr2
+    lea dx,msg2
+    mov ah,09h
+    int 21h 
+copy:          
     mov al,[si]
     mov [di],al
     inc si
@@ -21,23 +39,18 @@ copy:
     jnz copy
     
     mov cx,length
-    lea si,arr2
+    lea di,arr2
 print:
-    mov al,[si]
+    mov al,[di]    
     add al,'0'
     mov dl,al
     mov ah,02h
     int 21h
-    inc si
+    inc di
     dec cx
     jnz print
-    lea dx,msg
-    mov ah,09h
-    int 21h
+    
     mov ah,4ch
     int 21h
 code ends
 end start
-        
-    
-        
