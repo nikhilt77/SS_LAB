8de92caf754b7ff2bc036ac9fc36a8ba119e1fb2
Nikhil <130372520+nikhilt77@users.noreply.github.com>
2024-11-13 15:28:49 +0530

    Update pass1.c

--- pass1.c
+++ pass1.c
@@ -4,21 +4,20 @@
 #include<string.h>
 
 int main(){
-    char opcode[10],label[10],operand[10],mnemonic[10],code[10];
-    FILE *input,*output,*symtab,*optab,*symch;
-    int locctr,start;
-    bool err=false,found;
-
+    FILE *input,*output,*optab,*symtab,*symch;
     input=fopen("input.txt","r");
     optab=fopen("optab.txt","r");
-    symtab=fopen("symtab.txt","w");
     symch=fopen("symtab.txt","r");
-    output=fopen("output.txt","w");
+    symtab=fopen("symtab.txt","w");
+    output=fopen("intertab.txt","w");
+    int locctr,start;
+    bool err=false,found;
+    char label[10],opcode[10],operand[10],code[10],mnemonic[10];
 
     fscanf(input,"%s\t%s\t%s",label,opcode,operand);
-
     if(strcmp(opcode,"START")==0){
-        start=atoi(operand);
+        fseek(input,SEEK_SET,0);
+        fscanf(input,"%s\t%s\t%X",label,opcode,&start);
         locctr=start;
         fprintf(output,"-\t%s\t%s\t%s\n",label,opcode,operand);
         fscanf(input,"%s\t%s\t%s",label,opcode,operand);
@@ -30,108 +29,62 @@ int main(){
         if(label[0]=='#'){
             continue;
         }
-        fprintf(output,"%d\t",locctr);
+        fprintf(output,"%X\t",locctr);
         if(strcmp(label,"-")!=0){
-
             char symbol[10],loc[10];
             fseek(symch,SEEK_SET,0);
             fscanf(symch,"%s\t%s",symbol,loc);
             while(!feof(symch)){
-                if(strcmp(label,symbol)==0){
+                if(strcmp(symbol,label)==0){
                     err=true;
-                    printf("Duplicate Encountered\n");
+                    printf("Duplicate\n");
                     exit(0);
                 }
-                fscanf(symch,"%s\t%s",label,loc);
+                fscanf(symch,"%s\t%s",symbol,loc);
             }
-            fprintf(symtab,"%s\t%d\n",label,locctr);
+            fprintf(symtab,"%s\t%X\n",label,locctr);
         }
-        found=false;
         fseek(optab,SEEK_SET,0);
         fscanf(optab,"%s\t%s",code,mnemonic);
+        found=false;
         while(strcmp(code,"END")!=0){
-            if(strcmp(code,opcode)==0){
+            if(strcmp(opcode,code)==0){
                 locctr+=3;
                 found=true;
                 break;
             }
             fscanf(optab,"%s\t%s",code,mnemonic);
         }
-        if(strcmp(opcode,"WORD")==0){
-            locctr+=3;
-        }
-        else if(strcmp(opcode,"RESB")==0){
-            locctr+=atoi(operand);
-        }
-        else if(strcmp(opcode,"RESW")==0){
-            locctr+=3*(atoi(operand));
-        }
-        else if(strcmp(opcode,"RESB")==0){
-            locctr+=atoi(operand);
-        }
-        else if(strcmp(opcode,"BYTE")==0){
+        if(strcmp(opcode,"BYTE")==0){
             if(opcode[0]=='X'){
                 locctr+=1;
                 continue;
             }
             locctr+=strlen(operand)-3;
         }
+        else if(strcmp(opcode,"RESW")==0){
+            locctr+=3*atoi(operand);
+        }
+        else if(strcmp(opcode,"RESB")==0){
+            locctr+=atoi(operand);
+        }
+        else if(strcmp(opcode,"WORD")==0){
+            locctr+=3;
+        }
         else if(!found){
-            printf("Invalid\n");
             err=true;
+            printf("Invalid Opcode %s",opcode);
             exit(0);
         }
         fprintf(output,"%s\t%s\t%s\n",label,opcode,operand);
         fscanf(input,"%s\t%s\t%s",label,opcode,operand);
     }
-    fprintf(output,"-\t%s\t%s\t%s",label,opcode,operand);
-    printf("Length of code:%d\n",locctr-start);
-    fclose(input);
-    fclose(symtab);
-    fclose(optab);
+    fprintf(output,"%s\t%s\t%s\n",label,opcode,operand);
+    printf("Length:%d",locctr-start);
     fclose(symch);
+    fclose(symtab);
+    fclose(input);
     fclose(output);
+    fclose(optab);
+
 }
-/*
-SOURCE CODE:
-
--	START	2000
--	LDA FIVE
--	STA	ALPHA
--	LDCH 	CHARZ
-- 	STCH 	C1
-ALPHA 	RESW 	2
-FIVE 	WORD 	5
-CHARZ 	BYTE 	C'Z'
-C1 	RESB 	1
-- 	END	-
-
-OBJECT TABLE:
-
-LDA 03
-STA	0F
-LDCH	53
-STCH	57
-END -
-
-SYMBOL TABLE:
-
-ALPHA	2012
-FIVE	2018
-CHARZ	2021
-C1	2022
-INTERMEDIATE FILE:
-
--	-	START	2000
-2000	-	LDA	FIVE
-2003	-	STA	ALPHA
-2006	-	LDCH	CHARZ
-2009	-	STCH	C1
-2012	ALPHA	RESW	2
-2018	FIVE	WORD	5
-2021	CHARZ	BYTE	C'Z'
-2022	C1	RESB	1
--	-	END	-
-
-Length of code = 23
-*/
